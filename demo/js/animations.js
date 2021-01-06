a1c89cdaa1c4a511d023de2ea620f19ae3512af1
Evan Mitchell <etmitchell@bsu.edu>
2021-01-05 22:14:39 -0500

    First commit

---
+++ demo/js/animations.js
@@ -0,0 +1,30 @@
+
+// Welcome text
+$(window).scroll(function(){
+    var fromtop = $(document).scrollTop();      
+    $('.welcomeText h1').css({opacity: 320-fromtop}); 
+});
+
+$(window).scroll(function(){
+    var fromtop = $(document).scrollTop();      
+    $('.welcomeText h3').css({opacity: 390-fromtop}); 
+});
+
+$(window).scroll(function(){
+    var fromtop = $(document).scrollTop();      
+    $('.mouse').css({opacity: 390-fromtop}); 
+});
+
+
+// About Header fade
+$(window).scroll(function(){
+    var fromtop = $(document).scrollTop();      
+    $('.about-header').css({opacity: 1000-fromtop}); 
+});
+
+// Projects Header Fade
+$(window).scroll(function(){
+    var fromtop = $(document).scrollTop();      
+    $('.projects-header').css({opacity: 2050-fromtop}); 
+});
+
