45e3823b2b71cb1dc687756ce89f3acf8f490e38
Evan Mitchell <etmitchell@bsu.edu>
2021-01-09 00:05:23 -0500

    Added project and updated social icons

--- demo/js/animations.js
+++ demo/js/animations.js
@@ -1,30 +1,38 @@
+// // Welcome text
+// $(window).scroll(function () {
+//   var fromtop = $(document).scrollTop();
+//   $(".welcomeText h1").css({ opacity: 320 - fromtop });
+// });
 
-// Welcome text
-$(window).scroll(function(){
-    var fromtop = $(document).scrollTop();      
-    $('.welcomeText h1').css({opacity: 320-fromtop}); 
+// $(window).scroll(function () {
+//   var fromtop = $(document).scrollTop();
+//   $(".welcomeText h3").css({ opacity: 390 - fromtop });
+// });
+
+// $(window).scroll(function () {
+//   var fromtop = $(document).scrollTop();
+//   $(".mouse").css({ opacity: 390 - fromtop });
+// });
+
+// // About Header fade
+// $(window).scroll(function () {
+//   var fromtop = $(document).scrollTop();
+//   $(".about-header").css({ opacity: 1000 - fromtop });
+// });
+
+// // Projects Header Fade
+// $(window).scroll(function () {
+//   var fromtop = $(document).scrollTop();
+//   $(".projects-header").css({ opacity: 2050 - fromtop });
+// });
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 200) {
+      $("nav").css("background", "black");
+    } else {
+      $("nav").css("background", "transparent");
+    }
+  });
 });
-
-$(window).scroll(function(){
-    var fromtop = $(document).scrollTop();      
-    $('.welcomeText h3').css({opacity: 390-fromtop}); 
-});
-
-$(window).scroll(function(){
-    var fromtop = $(document).scrollTop();      
-    $('.mouse').css({opacity: 390-fromtop}); 
-});
-
-
-// About Header fade
-$(window).scroll(function(){
-    var fromtop = $(document).scrollTop();      
-    $('.about-header').css({opacity: 1000-fromtop}); 
-});
-
-// Projects Header Fade
-$(window).scroll(function(){
-    var fromtop = $(document).scrollTop();      
-    $('.projects-header').css({opacity: 2050-fromtop}); 
-});
-
